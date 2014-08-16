73137ddab266417a0a6aa379b684cb5084883c28
Colin Taylor <colint@smxemail.com>
2014-08-17 10:05:21 +1200

    release 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.smxemail/ccm-clj "0.2.0"
+(defproject com.smxemail/ccm-clj "0.1.3"
   :description "Clojure interface to Cassandra Cluster Manager"
   :min-lein-version "2.0.0"
   :url "https://github.com/SMX-LTD/ccm-clj"
