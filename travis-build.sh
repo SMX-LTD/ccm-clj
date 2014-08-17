7f614bff04328d62ffdc5bb64b08480efb1380d1
Colin Taylor <colint@smxemail.com>
2014-08-17 13:15:52 +1200

    travis take 2

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
-git clone https://github.com/pcmanus/ccm.git
+git clone https://github.com/pcmanus/ccm.git .
 sudo easy_install pyYaml
 sudo easy_install six
+cd ccm
 sudo ./setup.py install
